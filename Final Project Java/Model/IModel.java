a39ffba2002bc05998dbf4deac17b860973b5000
Itamar Yarden <itamary9@gmail.com>
2020-12-26 17:21:33 +0200

    Api documentation is fixed in IModel

--- Final Project Java/Model/IModel.java
+++ Final Project Java/Model/IModel.java
@@ -12,11 +12,16 @@ public interface IModel {
 
     public void createConnection() throws CostManagerException;
 
-    public void createDB() throws CostManagerException, SQLException;
+    public void createDB() throws CostManagerException;
 
 
     /*
      * Cost item DB execution
+     *
+     * API -
+     * @Post (addCostItem)  -  add new cost item to the DB.
+     * @Get (getCostItemBetweenDates) - get list of cost items between different dates.
+     * @Get (getCategorySumBetweenDates) - get list of sums of categories between dates
      */
 
     public void addCostItem(CostItem item) throws CostManagerException;
@@ -27,6 +32,9 @@ public interface IModel {
 
     /*
      * Category DB execution
+     *
+     * @Post (addCategory) - add new category to  DB.
+     * @Get (getAllCategory) - get all categories from DB.s
      */
 
     public void addCategory(Category category) throws CostManagerException;
